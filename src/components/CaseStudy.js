4ef0fa4385f32dfb3d1cadac420ee33b53672313
Dallen Hoyal <dallen.hoyal@gmail.com>
2022-03-07 23:24:22 -0700

    updates

--- src/components/CaseStudy.js
+++ src/components/CaseStudy.js
@@ -5,11 +5,24 @@ import colors from 'styles/colors'
 
 import RGB from 'images/RGB.png'
 
-const CaseStudy = ({ year, client, design, image }) => {
+const CaseStudy = ({ year, client, design, image, index, description }) => {
   return (
     <Wrapper image={image}>
       <Info>
-
+        <H2>> {`0${index + 1} _____Project`}</H2>
+        <Row>
+          <p>Year:</p>
+          <p>{year}</p>
+        </Row>
+        <Row>
+          <p>client:</p>
+          <p>{client}</p>
+        </Row>
+        <Row>
+          <p>design:</p>
+          <p>{design}</p>
+        </Row>
+        <P>{description}</P>
       </Info>
     </Wrapper>
   )
@@ -19,14 +32,25 @@ export default CaseStudy
 
 const Wrapper = styled.div`
   position: relative;
-  background-image: url(${RGB}), url(${props => props.image});
-  background-repeat: repeat, no-repeat;
-  background-size: 0.12vw, cover;
-  background-position: center center;
+  
   border: 2px solid ${colors.white};
   box-sizing: border-box;
-  filter: grayscale(1) blur(1px) drop-shadow(0vw 0vw 0.505vw #DCE8FF);
   width: 100%;
+
+  &::before {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    content: "";
+    top: 0;
+    left: 0;
+    background-image: url(${RGB}), url(${props => props.image});
+    background-repeat: repeat, no-repeat;
+    background-size: 0.2vw, cover;
+    background-position: center center;
+    filter: grayscale(1) blur(1px) drop-shadow(0vw 0vw 0.505vw #DCE8FF);
+    border-radius: 2.525vw;
+  }
 
   border-radius: 2.525vw;
   height: 49.091vw;
@@ -44,4 +68,58 @@ const Info = styled.div`
   height: 28.939vw;
   top: 2.02vw;
   right: 2.02vw;
+  padding: 2.5vw;
+`
+
+const H2 = styled.h2`
+  color: ${colors.purple};
+  font-family: Ofform;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 2.424vw;
+  line-height: 70%;
+  letter-spacing: -0.02em;
+
+  text-shadow: 0vw 0vw 0.253vw #E2E1FF;
+
+  margin-bottom: 2vw;
+`
+
+const Row = styled.div`
+  width: 100%;
+  border-bottom: 1px solid ${colors.white};
+  padding-bottom: 0.253vw;
+  // box-shadow: inset 0vw -0.051vw 0vw #EAE9E9;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 1vw;
+  filter: blur(0.5px);
+
+  p {
+    font-family: MD IO;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 1.212vw;
+    line-height: 100%;
+    letter-spacing: -0.02em;
+    color: ${colors.white};
+    text-shadow: 0vw 0vw 0.253vw #C1C0FF;
+  }
+`
+
+const P = styled.p`
+  font-family: MD IO;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 0.909vw;
+  line-height: 150%;
+  letter-spacing: -0.02em;
+
+  color: ${colors.white};
+
+  text-shadow: 0vw 0vw 0.253vw #C1C0FF;
+  filter: blur(0.5px);
+
+  margin-top: 1.253vw;
 `
--- src/sections/02-CaseStudies.js
+++ src/sections/02-CaseStudies.js
@@ -1,30 +1,63 @@
 import React, { useMemo } from 'react'
 import styled from 'styled-components'
 
-import colors from 'styles/colors'
-
 import CaseStudy from 'components/CaseStudy'
 
 import MiqrotechPNG from 'images/miqrotech.png'
+import MrAndMrsZPNG from 'images/mrandmrsz.png'
+import KielCummingsPNG from 'images/kielcummings.png'
 
 const DATA = [
   {
     year: 2021,
     client: "Miqrotech",
     design: "Reform Collective",
-    image: MiqrotechPNG
+    image: MiqrotechPNG,
+    description: `
+      I'm baby snackwave fixie swag, cornhole normcore 
+      raw denim bicycle rights pork belly. Man braid 
+      tacos sartorial vinyl neutra 3 wolf moon artisan,
+      succulents jean shorts. Franzen tote bag you 
+      probably haven't heard of them waistcoat 
+      crucifix salvia seitan. You probably haven't 
+      heard of them whatever bespoke dreamcatcher 
+      venmo YOLO four loko tattooed polaroid selvage 
+      pabst.
+    `
   },
   {
-    year: 2021,
-    client: "Miqrotech",
-    design: "Reform Collective",
-    image: MiqrotechPNG
+    year: 2022,
+    client: "Brandon Zacharias",
+    design: "Brandon Zacharias",
+    image: MrAndMrsZPNG,
+    description: `
+      I'm baby snackwave fixie swag, cornhole normcore 
+      raw denim bicycle rights pork belly. Man braid 
+      tacos sartorial vinyl neutra 3 wolf moon artisan,
+      succulents jean shorts. Franzen tote bag you 
+      probably haven't heard of them waistcoat 
+      crucifix salvia seitan. You probably haven't 
+      heard of them whatever bespoke dreamcatcher 
+      venmo YOLO four loko tattooed polaroid selvage 
+      pabst.
+    `
   },
   {
-    year: 2021,
-    client: "Miqrotech",
-    design: "Reform Collective",
-    image: MiqrotechPNG
+    year: 2022,
+    client: "Kiel Cummings",
+    design: "Kiel Cummings",
+    image: KielCummingsPNG,
+    description: `
+      I'm baby snackwave fixie swag, cornhole normcore 
+      raw denim bicycle rights pork belly. Man braid 
+      tacos sartorial vinyl neutra 3 wolf moon artisan,
+      succulents jean shorts. Franzen tote bag you 
+      probably haven't heard of them waistcoat 
+      crucifix salvia seitan. You probably haven't 
+      heard of them whatever bespoke dreamcatcher 
+      venmo YOLO four loko tattooed polaroid selvage 
+      pabst.
+    `
   },
 ]
 
@@ -33,13 +66,16 @@ const CaseStudies = () => {
   const projects = useMemo(() => DATA.map((item, index) => {
     return (
       <CaseStudy
+        key={index}
         year={item.year}
         client={item.client}
         design={item.design}
         image={item.image}
+        description={item.description}
+        index={index}
       />
     )
-  }), [DATA])
+  }), [])
 
   return (
     <Wrapper>
