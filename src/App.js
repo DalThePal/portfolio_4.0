bed8edbb99adf09a594478275600004a9228b51e
Dallen Hoyal <dallen.hoyal@gmail.com>
2022-08-30 21:08:48 -0600

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect } from 'react'
 import styled from 'styled-components'
 import { ScreenContext } from 'components/Providers'
-import axios from 'axios'
 
 import colors from 'styles/colors'
 import { Noise } from 'styles/uiElements'
